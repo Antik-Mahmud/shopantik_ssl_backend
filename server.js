255c607e2e21fdc724a816f2fe289226c12b6060
Antik-Mahmud <antikanimatedstudio@gmail.com>
2025-07-01 09:17:43 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Configure CORS
 const corsOptions = {
   origin: [
-    process.env.FRONTEND_URL || 'http://localhost:5173'
+    process.env.FRONTEND_URL
   ],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
